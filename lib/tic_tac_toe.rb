c23a3da81d2554876d00c9b10ec09fd877ba4205
Marc Steele <marcsteele2014@gmail.com>
2020-09-30 18:18:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -106,7 +106,7 @@ end
 def play(board)
   until over?(board)
     turn(board)
-    if won?(board) 
+    if won?(board)
       puts "Congratulations #{winner(board)}!"
     elsif draw?(board)
       puts "Cat's Game!"
